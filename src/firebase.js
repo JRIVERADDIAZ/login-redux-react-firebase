72c94ff388c6f727d61421a6c7e36b2ea2674afe
JRIVERADDIAZ <67973899+JRIVERADDIAZ@users.noreply.github.com>
2023-11-24 15:33:21 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYL5wG_2DUEGOK9vfQRmh7B7S7U1LRPE",
-    authDomain: "react-reducer-app-75782.firebaseapp.com",
-    projectId: "react-reducer-app-75782",
-    storageBucket: "react-reducer-app-75782.appspot.com",
-    messagingSenderId: "423461555217",
-    appId: "1:423461555217:web:efe1103ea2d430158f35b0",
-    measurementId: "G-JXL1YTY7F8"
+
 }
 
 export const app = initializeApp(firebaseConfig)
